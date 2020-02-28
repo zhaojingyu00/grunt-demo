8fc1d62bce0c57a8e801f5278d4a10934a9bacba
zhaojingyu00 <2819361152@qq.com>
2020-02-28 11:43:09 +0800

    csslint

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+      csslint: {
+        options: {
+          csslintrc: '.csslintrc'
+        },
+        src: ['*.css']
+      }
+    });
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.registerTask('default', ['csslint']);
+  };
